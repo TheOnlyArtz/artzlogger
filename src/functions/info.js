12dfba8dbcc878054e4119f38697ae7c7873237f
TheOnlyArtz <gamesil456@gmail.com>
2017-08-12 14:24:52 +0300

    Info

--- src/functions/info.js
+++ src/functions/info.js
@@ -1,5 +1,9 @@
 function info(input) {
+  if (Ooptions && Ooptions.timeStamp) {
+    console.log(Ooptions.timeStamp + ' ' + 'info'.green + ":", input.toString());
+  } else {
     console.log('info'.green + ":", input.toString());
+  }
 }
 
 module.exports = info;
