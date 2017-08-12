221c0daf27ff45514dc50d092873c7674e50ca16
TheOnlyArtz <gamesil456@gmail.com>
2017-08-12 14:28:57 +0300

    Added timestamps

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
 const Class = require('./classes/artzLogger');
-const logger = new Class({
-  timeStamp: moment(new Date).format('DD/MM/YYYY hh:mm:ss')
-})
-logger.rainbow('lol')
+
 module.exports = Class;
