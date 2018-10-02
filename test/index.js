8a881a34144b39f7d8c9dac9e306a9eeb10a3f1b
TheOnlyArtz <gamesil456@gmail.com>
2018-10-02 11:43:28 +0300

    Added tests

--- test/index.js
+++ test/index.js
@@ -4,10 +4,24 @@ const logger = new l({
   timestamp: () => {return moment().format('DD/MM/YYYY HH:mm:ss')},
   escapedOutput: true,
   capitalize: false,
+  streamLog: {
+      enabled: true,
+      encode: 'JSON',
+      path: "./logs/",
+  },
   decorators: {
       start: "[",
       end: "]"
   }
 });
 
-logger.error('Error!')
+logger.log('Log!');
+logger.error('Error!');
+logger.warn('Warn!');
+logger.info('Info!');
+logger.rainbow('Rainbow!');
+logger.debug('Debug!')
+
+setTimeout(() => {
+  logger.info('1 Sec later so it wont share the same timestamp.');
+}, 1000)
